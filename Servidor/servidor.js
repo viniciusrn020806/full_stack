50c190591909c3a7d580089f7190a28552a49e24
viniciusrn020806 <vinicius020806@gmail.com>
2025-04-25 20:28:29 -0300

    .

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -1,13 +1,39 @@
-var http= require('http');
+var http = require('http');
+var express = require('express');
+var colors = require('colors');
+var bodyParser = require('body-parser');
 
-var express=require('express');
-
-var app=express();
-
+var app = express();
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', './views');
 
-var server=http.createServer(app);
+var server = http.createServer(app);
+server.listen(80);
 
-server.listen(3000);
+console.log('Servidor rodando ...'.rainbow);
 
-console.log("servidor rodando...")
+app.get('/', function (requisicao, resposta){
+resposta.redirect('home.html')
+})
+
+app.get('/inicio', function (requisicao, resposta){
+var nome = requisicao.query.info;
+console.log(nome);
+})
+
+app.post('/inicio', function (requisicao, resposta){
+var data = requisicao.body.data;
+console.log(data);
+})
+
+app.get('/cadastro',function (requisicao, resposta){
+var nome = requisicao.query.nome;
+var sobrenome = requisicao.query.sobrenome;
+var nascimento = requisicao.query.nascimento;
+var civil = requisicao.query.civil;
+
+resposta.render('resposta_cadastro', {nome, sobrenome, nascimento, civil})
+})
