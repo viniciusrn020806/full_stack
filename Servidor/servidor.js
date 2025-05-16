a0bc126b2d7d9866d734beec1a913774b338a2d3
viniciusrn020806 <vinicius020806@gmail.com>
2025-05-16 02:35:15 -0300

    .

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -3,6 +3,16 @@ var express = require('express');
 var colors = require('colors');
 var bodyParser = require('body-parser');
 
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+
+const uri = "mongodb+srv://vinirn:upVEUPe0EAQlUbU7@vinirn.afjehzo.mongodb.net/?retryWrites=true&w=majority&appName=vinirn";
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
 var app = express();
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: false}))
@@ -14,6 +24,38 @@ var server = http.createServer(app);
 server.listen(80);
 
 console.log('Servidor rodando ...'.rainbow);
+const dbo = client.db("exemplo_bd");
+const usuarios = dbo.collection("usuarios");
+ 
+app.post("/cadastrar_usuario", function(req, resp) {
+    var data = { db_login: req.body.nome, db_senha: req.body.senha };
+
+    usuarios.insertOne(data, function (err) {
+      if (err) {
+        resp.render('resposta_usuario', {resposta: "Erro ao cadastrar usuário!"})
+      }else {
+        resp.render('resposta_usuario', {resposta: "Usuário cadastrado com sucesso!"})        
+      };
+    });
+   
+});
+ app.post("/logar_usuario", function(req, resp) {
+    var data = {db_login: req.body.login, db_senha: req.body.senha };
+
+    usuarios.find(data).toArray(function(err, items) {
+      console.log(items);
+      if (items.length == 0) {
+        resp.render('resposta_erro', {resposta: "Usuário/senha não encontrado!"})
+      }else if (err) {
+        resp.render('resposta_erro', {resposta: "Erro ao logar usuário!"})
+      }else {
+        resp.render('resposta_usuario', {resposta: "Usuário logado com sucesso!", nome: req.body.login})            
+      };
+    });
+
+  });
+
+
 
 app.get('/', function (requisicao, resposta){
 resposta.redirect('home.html')
@@ -29,7 +71,7 @@ var data = requisicao.body.data;
 console.log(data);
 })
 
-app.get('/cadastro',function (requisicao, resposta){
+app.post('/cadastro',function (requisicao, resposta){
 var nome = requisicao.query.nome;
 var sobrenome = requisicao.query.sobrenome;
 var nascimento = requisicao.query.nascimento;
