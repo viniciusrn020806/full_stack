ea16b02bbdaf39b98df22c19f38afd1662546305
viniciusrn020806 <vinicius020806@gmail.com>
2025-05-16 22:15:03 -0300

    lab 8 e 9

--- README.md
+++
@@ -1,3 +0,0 @@
-# full_stack# full_stack
-
-Repositório criado para adicionar as tarefas da diciplina full stack.
--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
-const uri = "mongodb+srv://vinirn:upVEUPe0EAQlUbU7@vinirn.afjehzo.mongodb.net/?retryWrites=true&w=majority&appName=vinirn";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = "mongodb+srv://vinirn:7cMdseBjLrEjvNlX@vinirn.afjehzo.mongodb.net/?retryWrites=true&w=majority&appName=vinirn";
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
 const app = express();
 app.use(express.static('./public'));
@@ -16,26 +16,51 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 const server = http.createServer(app);
-server.listen(3000, () => console.log('Servidor rodando na porta 3000...'.rainbow));
+server.listen(80, () => console.log('Servidor rodando na porta 80...'.rainbow));
 
 client.connect().then(() => {
   const dbo = client.db("exemplo_bd");
   const usuarios = dbo.collection("usuarios");
+  const posts = dbo.collection("posts"); // <- COLEÇÃO DE POSTS
 
-  // Rota para cadastrar novo usuário (usada no formulário)
+  // Página inicial redireciona para Projects.html
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/Projects.html");
+  });
+
+  // Página para exibir posts
+  app.get("/blog", (req, res) => {
+    posts.find().toArray((err, resultados) => {
+      if (err) return res.send("Erro ao buscar posts.");
+      res.render("blog", { posts: resultados });
+    });
+  });
+
+  // Página com formulário de criação
+  app.get("/cadastrar_post", (req, res) => {
+    res.sendFile(__dirname + "/public/cadastrar_post.html");
+  });
+
+  // Rota para salvar post
+  app.post("/cadastrar_post", (req, res) => {
+    const { titulo, resumo, conteudo } = req.body;
+    posts.insertOne({ titulo, resumo, conteudo }, (err, result) => {
+      if (err) return res.send("Erro ao salvar post.");
+      res.redirect("/blog");
+    });
+  });
+
+  // Rota para cadastrar novo usuário
   app.post("/cadastrar_usuario", (req, res) => {
     const nome = req.body.nome;
     const senha = req.body.senha;
-
     const data = { db_login: nome, db_senha: senha };
-    console.log('Tentando cadastrar usuário:', data);
 
     usuarios.insertOne(data, (err, result) => {
       if (err) {
         console.error('Erro ao cadastrar usuário:', err);
         res.render('resposta_usuario', { resposta: "Erro ao cadastrar usuário!" });
       } else {
-        console.log('Usuário cadastrado com sucesso. ID:', result.insertedId);
         res.render('resposta_usuario', { resposta: "Usuário cadastrado com sucesso!", nome });
       }
     });
@@ -45,19 +70,14 @@ client.connect().then(() => {
   app.post("/logar_usuario", (req, res) => {
     const nome = req.body.login;
     const senha = req.body.senha;
-
     const data = { db_login: nome, db_senha: senha };
-    console.log('Tentando logar usuário com:', data);
 
     usuarios.find(data).toArray((err, items) => {
       if (err) {
-        console.error('Erro ao logar usuário:', err);
         res.render('resposta_erro', { resposta: "Erro ao logar usuário!" });
       } else if (items.length === 0) {
-        console.log('Usuário/senha não encontrado para:', data);
         res.render('resposta_erro', { resposta: "Usuário/senha não encontrado!" });
       } else {
-        console.log('Usuário logado com sucesso:', items[0]);
         res.render('resposta_usuario', { resposta: "Usuário logado com sucesso!", nome });
       }
     });
